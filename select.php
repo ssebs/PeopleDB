47d7591af1667cf8ea8f1690a3b0d6c75a4ca2b8
Sebastian Safari <ssafari@ssebs-centos.ssebs.net>
2017-06-23 13:41:24 -0700

    Added modify/delete if search is only one user

--- select.php
+++ select.php
@@ -13,7 +13,6 @@
 </head>
 <body>
 	<h2>Search Results:</h2>
-	<hr>
 
 
 <?php
@@ -31,16 +30,51 @@ $sql = "SELECT * FROM Users WHERE user LIKE '$_user';";
 $result = $conn->query($sql);
 
 if($result->num_rows > 0) {
+	echo "<hr>";
+	echo "<h5 class='small-margin'>$result->num_rows results found</h5>";
+	echo "<hr><br>";
 	while($row = $result->fetch_assoc()) {
-		echo "User: " . $row['user'];
+		echo "<em>User: </em><strong>" . $row['user'] . "</strong>";
 		echo "<br>";
-		echo "First: " . $row['first'];
+		echo "<em>First: </em><strong>" . $row['first'] . "</strong>";
 		echo "<br>";
-		echo "Last: " . $row['last'];
+		echo "<em>Last: </em><strong>" . $row['last'] . "</strong>";
 		echo "<br>";
-		echo "Email: " . $row['email'];
+		echo "<em>Email: </em><strong>" . $row['email'] . "</strong>";
 		echo "<br>";
 		echo "<hr>";
+
+	if($result->num_rows == 1) {
+		echo '
+<form action="modify.php" method="post">
+	<fieldset>
+		<legend>Need to modify something?</legend>
+		';
+		echo '<p><label>User? <input type="text" name="user" value=' . $row["user"] . '></label></p>
+		
+		<p class="small-margin">Field to modify:</p>
+		<label class="inline"><input type="radio" name="field" value="user">User</label>
+		<label class="inline"><input type="radio" name="field" value="first">First</label>
+		<label class="inline"><input type="radio" name="field" value="last">Last</label>
+		<label class="inline"><input type="radio" name="field" value="email">Email</label>
+		
+		<p><label>Value? (what to change it to) <input type="text" name="value"></label></p>
+		<input type="submit">
+	</fieldset>
+</form>
+
+<form action="delete.php" method="post">
+	<fieldset>
+		<legend>Delete User</legend>
+';
+		echo '
+		<p><label>	Username? <input type="text" name="username" value="'. $row["user"] . '"></label></p>
+		<input type="submit">
+	</fieldset>
+</form>
+
+			';
+	}
 
 	}
 } else {
