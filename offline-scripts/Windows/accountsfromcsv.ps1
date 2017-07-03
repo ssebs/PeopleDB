9b20bb78bbcde3932d9718512a268765e5fb6f94
Sebastian Safari <ssafari@ssebs-centos.ssebs.net>
2017-07-03 08:43:47 -0700

    Added PS AD code

--- offline-scripts/Windows/accountsfromcsv.ps1
+++ offline-scripts/Windows/accountsfromcsv.ps1
@@ -10,14 +10,23 @@ foreach ($person in $peoplecsv) {
     $user = Get-ADUser -LDAPFilter "(sAMAccountName=$username)"
 
     If($user -ne $Null) {
-        echo "User: $username alreaady created"
-        continue;
+        echo "User: $username alreaady created, modifying..."
+        # Add where userid =, and a compare to see if vales are different.
+
+        Set-ADUser  -Identity $user `
+                    -DisplayName $name `
+                    -Surname $last `
+                    -GivenName $first `
+                    -SamAccountName "$username" `
+                    -UserPrincipalName "$email" `
+                    -EmailAddress $email
+        
     }Else {
         echo "Creating account for $username"
         
         $passwd = (ConvertTo-SecureString "SsebsP@ss" -AsPlainText -Force)
 
-         New-ADUser  -Name "$name" `
+         New-ADUser -Name "$username" `
                     -DisplayName $name `
                     -Surname $last `
                     -GivenName $first `
